55a8eb573f81b32ccadf6514b2777fa03761999a
Tom Purucker <purucker.tom@gmail.com>
2016-08-15 22:16:42 +0000

    line plot

---
+++ src/00setup.R
@@ -0,0 +1,33 @@
+#check to make sure required packages are installed
+list.of.packages <- c("ggplot2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)>0) {install.packages(new.packages)}
+
+#load library dependencies
+library(plyr)
+#library(reshape2)
+library(ggplot2)
+#library(grid)
+#library(gridExtra)
+#library(sensitivity)
+#library(abind)
+
+#echo environment
+Sys.info()
+Sys.info()[4]
+.Platform
+version
+
+#tom epa windows 2
+if(Sys.info()[4]=="DZ2626UTPURUCKE"){
+  ws_dir<-path.expand("k:/git/flaishansetal_watershedscaling/")
+}
+
+#subdirectories
+ws_dir_input <- paste(ws_dir, "csv_in/", sep = "")
+ws_dir_output <- paste(ws_dir, "output/", sep = "")
+ws_dir_figures <- paste(ws_dir, "figures/", sep = "")
+
+#run everything
+# define distributions for input parameters
+#source(paste(vpdir,"src/01parameterize_simulation.R",sep = ""))
---
+++ src/01processors_v_time.R
@@ -0,0 +1,53 @@
+#read input files
+#CONTROL
+tvp_filename <- paste(ws_dir_input, "time_v_processor.csv", sep = "")
+file.exists(tvp_filename)
+proc_v_time <- read.csv(file = tvp_filename, header = TRUE)
+
+dim(proc_v_time)
+summary(proc_v_time)
+#View(proc_v_time)
+
+#calculate base mean
+proc_1 <- proc_v_time[proc_v_time$processors==1,2]
+mean(proc_1)
+
+#get mean performance times
+mean_times <- ddply(proc_v_time, .(processors), summarize, mean_time=mean(time))[,2]
+base_mean <- mean_times[1]
+speedup <- base_mean/mean_times
+
+#add speedup
+proc_v_time$speedup <- proc_1/proc_v_time$time
+
+#fig1 <- ggplot(proc_v_time, aes(processors), speedup)
+#fig1 + geom_boxplot()
+
+#amdahl
+proc <- c(1,2,4,8,16,24,32)
+
+p50 = 0.5
+p75 = 0.75
+p90 = 0.9
+p95 = 0.95
+p99 = 0.99
+
+#1/(fixed + parallelized)
+l50 = 1/((1-p50) + p50/proc)
+l75 = 1/((1-p75) + p75/proc)
+l90 = 1/((1-p90) + p90/proc)
+l95 = 1/((1-p95) + p95/proc)
+l99 = 1/((1-p99) + p99/proc)
+
+amdahl <- data.frame(cbind(proc,l50,l75,l90,l95,l99))
+
+#line plot
+ggplot(amdahl, aes(proc)) + 
+  geom_line(aes(y = l99, colour = "99%")) +
+  geom_line(aes(y = l95, colour = "95%")) +
+  geom_line(aes(y = l90, colour = "90%")) +
+  geom_line(aes(y = l75, colour = "75%")) +
+  geom_line(aes(y = l50, colour = "50%"))  +
+  geom_line(aes(y = speedup, colour = "observed")) +
+  scale_fill_discrete(breaksreverse)
+
