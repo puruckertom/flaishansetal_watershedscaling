fe27fae65415a3bd016400a9d435caf54c77e216
Tom Purucker <purucker.tom@gmail.com>
2016-08-19 21:29:00 +0000

    lakes per bin figure

---
+++ src/03watershed_description.R
@@ -0,0 +1,16 @@
+#read input files
+#CONTROL
+ws_filename <- paste(ws_dir_input, "lakes_per_bin.csv", sep = "")
+file.exists(ws_filename)
+lakes_per_bin <- read.csv(file = ws_filename, header = TRUE)
+
+dim(lakes_per_bin)
+summary(lakes_per_bin)
+#View(lakes_per_bin)
+
+#ggplot(lakes_per_bin, aes(x=bin)) +
+#  geom_histogram()
+
+
+qplot(bin, data=lakes_per_bin, weight=Lakes, geom="histogram", xlab="Bin ID", ylab="Number of Lakes", binwidth=1) +
+  theme_bw()
