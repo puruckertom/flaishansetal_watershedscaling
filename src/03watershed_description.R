417fe1fae3c62e508a2a1f6bb849549fafa16926
Tom Purucker <purucker.tom@gmail.com>
2016-09-06 12:57:56 +0000

    watershed description figure

--- src/03watershed_description.R
+++ src/03watershed_description.R
@@ -16,7 +16,21 @@ qplot(bin, data=lakes_per_bin, weight=Lakes, geom="histogram", xlab="Bin ID",
       ylab="Number of Lakes", xlim=c(1,29), fill=I("blue"), col=I("blue"), binwidth=1) + 
   theme_bw()
 
-#reaches per lake
-ws_filename <- paste(ws_dir_input, "lakes_per_bin.csv", sep = "")
+#upstream reaches per lake
+ws_filename <- paste(ws_dir_input, "upstream_reaches_per_bin.csv", sep = "")
 file.exists(ws_filename)
-lakes_per_bin <- read.csv(file = ws_filename, header = TRUE)
+reaches_per_bin <- read.csv(file = ws_filename, header = TRUE)
+colnames(reaches_per_bin)
+summary(reaches_per_bin)
+
+pdf(file= paste(ws_dir_figures, "fig3_reaches_per_bin.pdf", sep=""), width = 8, height = 6)
+  ggplot(reaches_per_bin, aes(factor(bin), upstream_reaches, fill=factor(bin))) +
+    geom_boxplot()+
+    scale_x_discrete(labels=1:29) +
+    #scale_fill_manual(values = c("yellow", "orange")) +
+    xlab("Bin Number") + 
+    ylab("Upstream Reaches") +
+    ggtitle("") +
+    guides(fill=FALSE) +
+    theme_bw()
+dev.off()
